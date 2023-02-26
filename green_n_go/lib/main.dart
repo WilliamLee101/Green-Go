cdab72c649f74a54ffea3d3f2c7875994f9f060a
Tyler Nguyen <123tylernguyen@gmail.com>
2023-02-26 15:45:35 -0500

    can read from database now

--- green_n_go/lib/main.dart
+++ green_n_go/lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:green_n_go/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
+FirebaseDatabase database = FirebaseDatabase.instance;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- green_n_go/lib/warren.dart
+++ green_n_go/lib/warren.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class Warren extends StatefulWidget {
@@ -8,12 +10,23 @@ class Warren extends StatefulWidget {
 }
 
 class _WarrenState extends State<Warren> {
+  DatabaseReference warrenRef =
+      FirebaseDatabase.instance.ref().child("updated_menu/2023-02-25/breakfast");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Warren Menu'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Warren Menu'),
+        ),
+        body: Container(
+          height: double.infinity,
+          child: FirebaseAnimatedList(
+              query: warrenRef,
+              itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                  Animation<double> animation, int index) {
+                return Text('hello');
+              }),
+        ));
   }
 }
