a4868dcf6a04607e620fd24aea55afce1c045646
Tyler Nguyen <123tylernguyen@gmail.com>
2023-03-12 19:46:29 -0400

    latest code

--- green_n_go/lib/warren.dart
+++ green_n_go/lib/warren.dart
@@ -10,26 +10,31 @@ class Warren extends StatefulWidget {
 }
 
 class _WarrenState extends State<Warren> {
+  DateTime date = DateTime.now();
   DatabaseReference warrenRefBF = FirebaseDatabase.instance
       .ref()
-      .child("updated_menu/2023-02-27/breakfast");
+      .child("updated_menu/2023-03-13/breakfast");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Warren Menu'),
-        ),
-        body: Container(
-            height: double.infinity,
-            child: FirebaseAnimatedList(
-                query: warrenRefBF,
-                itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                    Animation<double> animation, int index) {
-                  return Center(
-                    child: Container(decoration: BoxDecoration(color:Colors.white54, border: Border.all(color: Colors.black26)),child: Text(snapshot.child('item').value.toString())),
-                  );
-                }),
-          ),
-        );
+      appBar: AppBar(
+        title: const Text('Warren Menu'),
+      ),
+      body: Container(
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+            query: warrenRefBF,
+            itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                Animation<double> animation, int index) {
+              return Center(
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white54,
+                        border: Border.all(color: Colors.black26)),
+                    child: Text(snapshot.child('item').value.toString())),
+              );
+            }),
+      ),
+    );
   }
 }
