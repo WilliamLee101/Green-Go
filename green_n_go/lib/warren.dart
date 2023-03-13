f309abdb90a9cb74b140e655213ced8d925d8f51
Tyler Nguyen <123tylernguyen@gmail.com>
2023-03-12 22:42:17 -0400

    Can pull breakfast, lunch, and dinner menus now

--- green_n_go/lib/warren.dart
+++ green_n_go/lib/warren.dart
@@ -2,6 +2,89 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
+class FoodItem {
+  final String name;
+  final String? carbs;
+  final String? protiens;
+  final String? satFat;
+  final String? sugars;
+  final String? cals;
+
+  const FoodItem(
+      {required this.name,
+      this.carbs,
+      this.protiens,
+      this.satFat,
+      this.sugars,
+      this.cals});
+}
+
+DateTime date = DateTime.now();
+final List<FoodItem> bmenu = [];
+final List<FoodItem> lmenu = [];
+final List<FoodItem> dmenu = [];
+// DatabaseReference warrenRefBF = FirebaseDatabase.instance
+//     .ref()
+//     .child("updated_menu/2023-03-13/warren/breakfast");
+final ref = FirebaseDatabase.instance.ref();
+Future<void> getMenu() async {
+  final snapshot =
+      await ref.child("updated_menu/2023-03-13/warren/breakfast").get();
+  if (snapshot.exists && snapshot.value is Map<dynamic, dynamic>) {
+    (snapshot.value as Map<dynamic, dynamic>).forEach((key, value) {
+      final food = FoodItem(
+        name: value['item'],
+        carbs: value['carbs'],
+        protiens: value['proteins'],
+        satFat: value['saturated fat'],
+        sugars: value['sugars'],
+        cals: value['cals'],
+      );
+
+      bmenu.add(food);
+    });
+  } else {
+    print('No data available.');
+  }
+  final snapshot1 =
+      await ref.child("updated_menu/2023-03-13/warren/lunch").get();
+  if (snapshot1.exists && snapshot1.value is Map<dynamic, dynamic>) {
+    (snapshot1.value as Map<dynamic, dynamic>).forEach((key, value) {
+      final food = FoodItem(
+        name: value['item'],
+        carbs: value['carbs'],
+        protiens: value['proteins'],
+        satFat: value['saturated fat'],
+        sugars: value['sugars'],
+        cals: value['cals'],
+      );
+
+      lmenu.add(food);
+    });
+  } else {
+    print('No data available.');
+  }
+  final snapshot2 =
+      await ref.child("updated_menu/2023-03-13/warren/dinner").get();
+  if (snapshot2.exists && snapshot2.value is Map<dynamic, dynamic>) {
+    (snapshot2.value as Map<dynamic, dynamic>).forEach((key, value) {
+      final food = FoodItem(
+        name: value['item'],
+        carbs: value['carbs'],
+        protiens: value['proteins'],
+        satFat: value['saturated fat'],
+        sugars: value['sugars'],
+        cals: value['cals'],
+      );
+      print(food.name);
+      print(food.cals);
+      dmenu.add(food);
+    });
+  } else {
+    print('No data available.');
+  }
+}
+
 class Warren extends StatefulWidget {
   const Warren({super.key});
 
@@ -10,31 +93,28 @@ class Warren extends StatefulWidget {
 }
 
 class _WarrenState extends State<Warren> {
-  DateTime date = DateTime.now();
-  DatabaseReference warrenRefBF = FirebaseDatabase.instance
-      .ref()
-      .child("updated_menu/2023-03-13/breakfast");
   @override
   Widget build(BuildContext context) {
+    getMenu();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Warren Menu'),
       ),
-      body: Container(
-        height: double.infinity,
-        child: FirebaseAnimatedList(
-            query: warrenRefBF,
-            itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                Animation<double> animation, int index) {
-              return Center(
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white54,
-                        border: Border.all(color: Colors.black26)),
-                    child: Text(snapshot.child('item').value.toString())),
-              );
-            }),
-      ),
+      // body: Container(
+      //   height: double.infinity,
+      //   child: FirebaseAnimatedList(
+      //       query: warrenRefBF,
+      //       itemBuilder: (BuildContext context, DataSnapshot snapshot,
+      //           Animation<double> animation, int index) {
+      //         return Center(
+      //           child: Container(
+      //               decoration: BoxDecoration(
+      //                   color: Colors.white54,
+      //                   border: Border.all(color: Colors.black26)),
+      //               child: Text(snapshot.child('item').value.toString())),
+      //         );
+      //       }),
+      // ),
     );
   }
 }
